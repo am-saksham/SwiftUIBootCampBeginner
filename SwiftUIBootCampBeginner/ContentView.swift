8129f11cbf1e07f2f6547f1a4cca4afd781937af
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-09-14 18:27:41 +0530

    Initial Commit

---
+++ SwiftUIBootCampBeginner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIBootCampBeginner
+//
+//  Created by Saksham Gupta on 14/09/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIBootCampBeginner/SwiftUIBootCampBeginnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIBootCampBeginnerApp.swift
+//  SwiftUIBootCampBeginner
+//
+//  Created by Saksham Gupta on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIBootCampBeginnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
