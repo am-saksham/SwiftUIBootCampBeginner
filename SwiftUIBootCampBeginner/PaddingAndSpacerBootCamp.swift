9e32f020344b05a947ddd9e56449b38c77f8fc54
Saksham <saksham.23bce10006@vitbhopal.ac.in>
2024-12-03 12:33:42 +0530

    added padding and spacer file

---
+++ SwiftUIBootCampBeginner/PaddingAndSpacerBootCamp.swift
@@ -0,0 +1,18 @@
+//
+//  PaddingAndSpacerBootCamp.swift
+//  SwiftUIBootCampBeginner
+//
+//  Created by Saksham Gupta on 03/12/24.
+//
+
+import SwiftUI
+
+struct PaddingAndSpacerBootCamp: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    PaddingAndSpacerBootCamp()
+}
